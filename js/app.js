37d3b095a3f33f4fd187bdea92f92971800e5bd6
Michael Neely <michael@michaelneely.net>
2020-05-01 23:41:07 -0400

    Added teh calculations for the car insurance

--- js/app.js
+++ js/app.js
@@ -1,20 +1,81 @@
 // Variables
-
-
+const form = document.getElementById('request-quote'); 
+const html = new HTMLUI;
 
 
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', function() {
-    // Create the <options? fo the years
-    const html = new HTMLUI;
-    html.displayYears();
+eventListeners();
+function eventListeners() {
+        document.addEventListener('DOMContentLoaded', function() {
+        // Create the <options? fo the years
+        
+        html.displayYears();
 
-});
+    });
+
+        // When form is submitted.
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            const make = document.getElementById('make').value;
+            const year = document.getElementById('year').value;
+
+            const level = document.querySelector('input[name="level"]:checked').value;
+
+            // Check that all the fields have something
+            if(make === '' || year === '' || level ===- '') {
+                html.displayError('All the fields are mandatory')
+            } else {
+                const insurance = new Insurance(make, year, level);
+                const price = insurance.calculateQuotation(insurance);
+            }
+        });
+
+}
+
+// Everything related to the calculation
+function Insurance(make, year, level) {
+    this.make = make;
+    this.year= year;
+    this.level = level;
+
+}
+
+// Calculate the price for the current quotation
+Insurance.prototype.calculateQuotation = function(insurance) {
+    let price;
+    const base = 2000;
+
+    const make = insurance.make;
+
+    switch(make) {
+        case '1':
+            price = base * 1.15;
+            break;
+        case '2':
+            price = base * 1.05;
+            break;
+        case '3':
+            price = base * 1.35;
+            break;
+    }
+    
+    const year = insurance.year;
+    const difference = this.getYearDifference(year);
+
+    console.log(difference);
+    // Each year the cost of insurance is going to be 3% cheaper
+    price = price - ((difference * 3) * price) /100;
+
+    console.log(price);
 
 
+}
 
-
+Insurance.prototype.getYearDifference = function(year) {
+    return new Date().getFullYear() - this.year;
+}
 
 // Objects
 function HTMLUI() {
@@ -25,7 +86,7 @@ function HTMLUI() {
 HTMLUI.prototype.displayYears = function() {
     // Max and minumium years
     const max = new Date().getFullYear(), 
-            min = max - 20;;
+            min = max - 20;
 
     console.log(max);
     console.log(min);
@@ -42,3 +103,19 @@ HTMLUI.prototype.displayYears = function() {
     }
 }
 
+HTMLUI.prototype.displayError = function(message) {
+
+    const div = document.createElement('div');
+
+    div.classList = 'error';
+
+    div.innerHTML = `
+        <p>${message}</p>
+    `;
+    form.insertBefore(div, document.querySelector('.form-group'));
+
+    // REmove error
+    setTimeout(function() {
+        document.querySelector('.error').remove();
+    }, 3000);
+}
